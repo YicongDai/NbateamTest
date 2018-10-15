88ca3ad1afaa815c2777d8a4d5fa98bc29ef0fc9
Yicong Dai <252332082@qq.com>
2018-10-16 05:51:52 +0800

    Initial Structure of team schema

---
+++ models/team.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+
+let TeamSchema = new mongoose.Schema({
+        name: String,
+        city: String,
+        zone:{name:String,location:String},
+        numPlayer: Number,
+        championships:Number,
+        rank:Number
+
+    },
+    { collection: 'team' });
+
+module.exports = mongoose.model('team', TeamSchema );
